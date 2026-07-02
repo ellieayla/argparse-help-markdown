f502cdc6d888fbe9329a510593bfd12924f05230
ellieayla <1447600+me@users.noreply.github.com>
2026-07-02 19:29:55 -0400

    Add support for choice=StrEnum instances.
    A StrEnum subclass can be used as an iterable for the add_argument(choices=...) parameter.

---
+++ tests/data/choice_enum.py
@@ -0,0 +1,21 @@
+from argparse import ArgumentParser
+from enum import StrEnum
+
+
+class EnumChoice(StrEnum):
+    """Requesting something"""
+
+    yes = "yes"
+    no = "no"
+
+
+def main() -> None:
+    p = ArgumentParser(prog="Example")
+    p.add_argument("--enum", choices=EnumChoice, help="c")
+
+    _ = p.parse_args()
+    raise ValueError("Should never reach here.")  # pragma: no cover
+
+
+if __name__ == "__main__":
+    main()
---
+++ tests/test_choice_enum.py
@@ -0,0 +1,22 @@
+from pathlib import Path
+
+import pytest
+
+from argparse_help_markdown import run
+
+FIXTURE_DIR = Path(__file__).parent.resolve() / "data"
+
+
+@pytest.mark.datafiles(FIXTURE_DIR / "choice_enum.py")
+def test_choice_enum(datafiles: Path, capsys: pytest.CaptureFixture[str]) -> None:
+    assert datafiles.is_dir()
+
+    subject: Path = datafiles / "choice_enum.py"
+
+    run(filename=str(subject.absolute()), include_usage=False, writer=None)
+
+    captured = capsys.readouterr()
+
+    assert "--enum" in captured.out
+    assert "Optional." in captured.out
+    assert "Choice: `yes`, `no`" in captured.out
